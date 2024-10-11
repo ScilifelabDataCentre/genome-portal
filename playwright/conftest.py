d740f2e2dfc4db44d1b331cf563aa189710fb29f
RMCrean <RMCrean@users.noreply.github.com>
2024-10-11 16:30:01 +0200

    add tests for glossary page

--- playwright/conftest.py
+++ playwright/conftest.py
@@ -1,20 +1,6 @@
 import pytest
 
-from playwright.sync_api import Page
-
 
 @pytest.fixture(scope="session")
 def base_url():
     return "http://localhost:1313"
-
-
-@pytest.fixture
-def home_page(page: Page, base_url: str) -> Page:
-    page.goto(base_url)
-    return page
-
-
-@pytest.fixture
-def about_page(page: Page, base_url: str) -> Page:
-    page.goto(base_url + "/about/")
-    return page
