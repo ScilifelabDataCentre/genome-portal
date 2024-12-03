ScilifelabDataCentre/genome-portal

commit dc1908942c935cdd0821474fa2e08262bd897b9a

[skip ci] Correctly handle commma-separated SPECIES list

Makefile
@@ -15,9 +15,7 @@ INSTALL_DIR := $(SWG_INSTALL_DIR)
 # comma-separated list to the SPECIES variable. Example:
 #
 #     make SPECIES=linum_tenue,clupea_harengus build
-SPECIES := $(SPECIES:%:{%})
-
-CONFIGS := $(shell find $(CONFIG_DIR)/$(SPECIES) -type f -name 'config.yml')
+CONFIGS := $(shell find $(CONFIG_DIR)/$(SPECIES:%={%}) -type f -name 'config.yml')
 JBROWSE_CONFIGS := $(patsubst $(CONFIG_DIR)/%,$(DATA_DIR)/%,$(CONFIGS:.yml=.json))
 
 # Files to download for further processing (typically compressing and

commit eb0eb05f76e8a0ccd6e3415ec657d1948c60d61d

[skip ci] Revert "Correctly handle commma-separated SPECIES list"
This reverts commit dc1908942c935cdd0821474fa2e08262bd897b9a,
which broke the single-species use case: SPECIES=some_species

Makefile
@@ -15,7 +15,9 @@ INSTALL_DIR := $(SWG_INSTALL_DIR)
 # comma-separated list to the SPECIES variable. Example:
 #
 #     make SPECIES=linum_tenue,clupea_harengus build
-CONFIGS := $(shell find $(CONFIG_DIR)/$(SPECIES:%={%}) -type f -name 'config.yml')
+SPECIES := $(SPECIES:%:{%})
+
+CONFIGS := $(shell find $(CONFIG_DIR)/$(SPECIES) -type f -name 'config.yml')
 JBROWSE_CONFIGS := $(patsubst $(CONFIG_DIR)/%,$(DATA_DIR)/%,$(CONFIGS:.yml=.json))
 
 # Files to download for further processing (typically compressing and