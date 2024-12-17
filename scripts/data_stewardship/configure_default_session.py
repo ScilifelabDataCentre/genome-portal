fcadc238a589b34d641629ae3643f4961b0a8f1f
Daniel P Brink <daniel.brink@scilifelab.se>
2024-12-17 16:18:55 +0100

    Add docstring to functions and miscellaneous polish

--- scripts/data_stewardship/configure_default_session.py
+++ scripts/data_stewardship/configure_default_session.py
@@ -21,7 +21,7 @@ The file or its content can then be copied to config.json in .config/[SPECIES_NA
 
 ### Dependencies:
 pyyaml                          -   Used for parsing the yaml file.
-                                    Can be for instance be installed with pip: pip install pyyaml
+                                    Can be for instance be installed with pip: `pip install pyyaml`
 
 ### Usage:
 (Assuming that the following two steps have already been completed:
@@ -54,7 +54,7 @@ import yaml
 
 def parse_arguments():
     """
-    Run argparse to set up and capture the arguments of the script.
+    Subfunction that runs argparse to set up and capture the arguments of the script.
     """
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
@@ -80,6 +80,11 @@ def parse_arguments():
 
 
 def get_git_repo_root():
+    """
+    Subfunction that checks if the script is run from within a git repository and, if so, returns the root directory of the repository.
+    The path to the root directory is used to locate in other subfunctions to locate the assembly files and to set a default directory for
+    saving the output JSON file.
+    """
     try:
         git_root = subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).strip().decode("utf-8")
         return git_root
@@ -91,6 +96,10 @@ def get_git_repo_root():
 
 
 def get_species_abbreviation(organism):
+    """
+    Subfunction that takes the scientific (binomial) species name and returns an abbreviation that will be used as a track id suffix.
+    Example: "Linum tenue" -> "lten". In order for this to work, the organism name need to be a non-empty string; this is checked in the main() function.
+    """
     words = organism.split()
     if len(words) >= 2:
         return (words[0][0] + words[1][:3]).lower()
@@ -99,6 +108,13 @@ def get_species_abbreviation(organism):
 
 
 def get_fasta_header_and_sequence_length(file_path, default_scaffold=None):
+    """
+    Subfunction that reads an assembly FASTA file and returns the header of the first scaffold and its sequence length.
+    Alternatively, if the user has defined a default scaffold in the config.yml with the defaultScaffold key,
+    the function will return the header of that scaffold instead of the first scaffold. The header and length are used to
+    populate the defaultSession JSON object to control which scaffold is display upon loading a new session and its zoom level.
+    """
+
     def parse_fasta_file(file, default_scaffold):
         first_fasta_header = None
         sequence_length = 0
@@ -157,6 +173,10 @@ def get_protein_coding_genes_file_name(config):
 
 
 def populate_defaultSession_object(data, species_info):
+    """
+    Subfunction that populates the outer parts of the defaultSession JSON object with the species name and abbreviation.
+    Also adds disableAnalytics to the dictionary to prevent Google Analytics from being loaded when running JBrowse.
+    """
     data["defaultSession"] = {
         "id": f"{species_info["species_abbreviation"]}_default_session",
         "name": species_info["species_name"],
@@ -175,6 +195,13 @@ def populate_defaultSession_object(data, species_info):
 
 
 def initiate_views_and_populate_mandatory_tracks(data, species_info, config, git_root, config_dir, assembly_counter):
+    """
+    Subfunction that adds a JBrowse view to the defaultSession JSON object. The funcion is iterated upon by main()
+    so that a new view is added for each assembly in the config.yml. It then checks if the primary assembly has a
+    protein-coding genes track, and if not, raises an error. For the secondary assemblies, there is no requirement to
+    have a protein-coding genes track, but at least one track is required. (The populate_values_from_optional_tracks()
+    is later called by main() to also ensure that the secondary assemblies have at least one track set to defaultSession: true.)
+    """
     species_abbreviation = species_info["species_abbreviation"]
     species_name_underscored = species_info["species_name_underscored"]
     assembly_file_name = os.path.basename(config["assembly"]["url"])
@@ -218,7 +245,7 @@ def initiate_views_and_populate_mandatory_tracks(data, species_info, config, git
         for track in config.get("tracks", [])
     )
 
-    # Primary assemblies are required to have a protein-coding genes track. This is added to the first view.
+    # Primary assemblies are required to have a protein-coding genes track; it is optional for secondary assemblies.
     if has_protein_coding_genes_track_in_config:
         protein_coding_gene_file_name = get_protein_coding_genes_file_name(config)
         protein_coding_genes_track = [
@@ -260,6 +287,102 @@ def initiate_views_and_populate_mandatory_tracks(data, species_info, config, git
 
 
 def populate_values_from_optional_tracks(config, data, species_abbreviation, assembly_counter):
+    """
+    Subfunction that handles the defaultSession JSON object with tracks that have the defaultSession flag set to true. It does this by
+    calling on a nested subfunction named add_defaultSession_true_tracks().
+
+    It also supports tracks that are set to be treated as GWAS tracks. At the time this script was written, the Genome Portal
+    makefile did not support adding GWAS tracks to config.json via the JBrowse CLI tools. Thus, such tracks are handled by the nested
+    subfunction add_gwas_true_tracks()
+    """
+
+    def add_defaultSession_true_tracks(track, data, species_abbreviation, assembly_counter):
+        track_outer_id = (
+            f"{species_abbreviation}_default_{track['name'].replace(' ', '_').replace('\'', '').replace(',', '')}"
+        )
+        track_file_name = f"{track['fileName'].rstrip('.gz').rstrip('.zip').rstrip('.bgz')}"
+        track_type = "LinearBasicDisplay"
+        track_config = track_file_name
+        display_config = f"{track_file_name}-LinearBasicDisplay"
+
+        # For the case when a track is true for both the defaultSession and GWAS keys:
+        if "GWAS" in track and track["GWAS"]:
+            track_type = "LinearManhattanDisplay"
+            # this next line needs can be improved: it serves to to reproduce the same output as the old handcrafted config.json for L. tenue.
+            # (the line is based on the track_outer_id form the GWAS function below)
+            track_config = (
+                f"{species_abbreviation}_init_{track['name'].replace(' ', '_').replace('\'', '').replace(',', '')}"
+            )
+            display_config = f"{track_config}_display"
+
+        new_track = {
+            "id": track_outer_id,
+            "type": "FeatureTrack",
+            "configuration": track_config,
+            "minimized": False,
+            "displays": [
+                {
+                    "id": f"{track_outer_id}_display",
+                    "type": track_type,
+                    "heightPreConfig": 150,
+                    "configuration": display_config,
+                }
+            ],
+        }
+        data["defaultSession"]["views"][assembly_counter]["tracks"].append(new_track)
+
+        return data
+
+    def add_gwas_true_tracks(track, data, species_abbreviation, assembly_name):
+        if "scoreColumnGWAS" not in track:
+            raise ValueError(
+                f"Track '{track['name']}' is configured to be treated as a GWAS track but is missing 'scoreColumnGWAS' in the config.yml. "
+                "Please update this and re-run the script."
+            )
+        adapter_scoreColumn = track["scoreColumnGWAS"]
+
+        track_outer_id = (
+            f"{species_abbreviation}_init_{track['name'].replace(' ', '_').replace('\'', '').replace(',', '')}"
+        )
+
+        def get_base_extension(file_name):
+            base_name = os.path.splitext(file_name)[0] if file_name.endswith((".gz", ".zip")) else file_name
+            return os.path.splitext(base_name)[1].lstrip(".")
+
+        base_extension = get_base_extension(track["fileName"])
+
+        # Eventually we need to support more file types here as we encounter them.
+        # There is also a discussion to be had if the makefile should handle GWAS files instead of this script
+        if base_extension == "bed":
+            adapter_type = "BedTabixAdapter"
+            bed_gz_location = track["fileName"]
+            if bed_gz_location.endswith((".gz", ".zip")):
+                bed_gz_location = bed_gz_location.replace(".gz", ".bgz").replace(".zip", ".bgz")
+            index_location = f"{bed_gz_location}.tbi"
+
+        # category is hardcoded for now, but could be added to the config.yml in the future
+        # An added benefit of having the category is that it ensures that the tracks come below the protein-codon genes in the track selector
+        new_GWAS_track = {
+            "type": "FeatureTrack",
+            "trackId": track_outer_id,
+            "name": track["name"],
+            "assemblyNames": [assembly_name],
+            "category": ["GWAS"],
+            "adapter": {
+                "type": adapter_type,
+                "scoreColumn": adapter_scoreColumn,
+                "bedGzLocation": {"uri": bed_gz_location},
+                "index": {"location": {"uri": index_location}},
+            },
+            "displays": [{"displayId": f"{track_outer_id}_display", "type": "LinearManhattanDisplay"}],
+        }
+
+        if "tracks" not in data:
+            data["tracks"] = []
+        data["tracks"].append(new_GWAS_track)
+
+        return data
+
     plugin_added = False
     assembly_name = config.get("assembly", {}).get("name", "")
 
@@ -272,7 +395,7 @@ def populate_values_from_optional_tracks(config, data, species_abbreviation, ass
         for track in data["defaultSession"]["views"][assembly_counter]["tracks"]
     )
 
-    if has_at_least_one_default_session_flag:
+    if has_at_least_one_default_session_flag or has_protein_coding_genes_track_in_dict:
         for track in config["tracks"]:
             if "defaultSession" in track and track["defaultSession"]:
                 # Ensure protein-coding genes are not added to the default session again if the user has happened to set it with defaultSession: true in the config.yml
@@ -301,110 +424,47 @@ def populate_values_from_optional_tracks(config, data, species_abbreviation, ass
     return data
 
 
-def add_defaultSession_true_tracks(track, data, species_abbreviation, assembly_counter):
-    track_outer_id = (
-        f"{species_abbreviation}_default_{track['name'].replace(' ', '_').replace('\'', '').replace(',', '')}"
-    )
-    track_file_name = f"{track['fileName'].rstrip('.gz').rstrip('.zip').rstrip('.bgz')}"
-    track_type = "LinearBasicDisplay"
-    track_config = track_file_name
-    display_config = f"{track_file_name}-LinearBasicDisplay"
-
-    # For the case when a track is true for both the defaultSession and GWAS keys:
-    if "GWAS" in track and track["GWAS"]:
-        track_type = "LinearManhattanDisplay"
-        # this next line needs can be improved: it serves to to reproduce the same output as the old handcrafted config.json for L. tenue.
-        # (the line is based on the track_outer_id form the GWAS function below)
-        track_config = (
-            f"{species_abbreviation}_init_{track['name'].replace(' ', '_').replace('\'', '').replace(',', '')}"
-        )
-        display_config = f"{track_config}_display"
-
-    new_track = {
-        "id": track_outer_id,
-        "type": "FeatureTrack",
-        "configuration": track_config,
-        "minimized": False,
-        "displays": [
-            {
-                "id": f"{track_outer_id}_display",
-                "type": track_type,
-                "heightPreConfig": 150,
-                "configuration": display_config,
-            }
-        ],
-    }
-    data["defaultSession"]["views"][assembly_counter]["tracks"].append(new_track)
-
-    return data
-
-
-def add_gwas_true_tracks(track, data, species_abbreviation, assembly_name):
-    if "scoreColumnGWAS" not in track:
-        raise ValueError(
-            f"Track '{track['name']}' is configured to be treated as a GWAS track but is missing 'scoreColumnGWAS' in the config.yml. Please update this and re-run the script."
-        )
-    adapter_scoreColumn = track["scoreColumnGWAS"]
-
-    track_outer_id = f"{species_abbreviation}_init_{track['name'].replace(' ', '_').replace('\'', '').replace(',', '')}"
-
-    def get_base_extension(file_name):
-        base_name = os.path.splitext(file_name)[0] if file_name.endswith((".gz", ".zip")) else file_name
-        return os.path.splitext(base_name)[1].lstrip(".")
-
-    base_extension = get_base_extension(track["fileName"])
-
-    # Eventually we need to support more file types here as we encounter them.
-    # There is also a discussion to be had if the makefile should handle GWAS files instead of this script
-    if base_extension == "bed":
-        adapter_type = "BedTabixAdapter"
-        bed_gz_location = track["fileName"]
-        if bed_gz_location.endswith((".gz", ".zip")):
-            bed_gz_location = bed_gz_location.replace(".gz", ".bgz").replace(".zip", ".bgz")
-        index_location = f"{bed_gz_location}.tbi"
-
-    # category is hardcoded for now, but could be added to the config.yml in the future
-    # An added benefit of having the category is that it ensures that the tracks come below the protein-codon genes in the track selector
-    new_GWAS_track = {
-        "type": "FeatureTrack",
-        "trackId": track_outer_id,
-        "name": track["name"],
-        "assemblyNames": [assembly_name],
-        "category": ["GWAS"],
-        "adapter": {
-            "type": adapter_type,
-            "scoreColumn": adapter_scoreColumn,
-            "bedGzLocation": {"uri": bed_gz_location},
-            "index": {"location": {"uri": index_location}},
-        },
-        "displays": [{"displayId": f"{track_outer_id}_display", "type": "LinearManhattanDisplay"}],
-    }
-
-    if "tracks" not in data:
-        data["tracks"] = []
-    data["tracks"].append(new_GWAS_track)
-
-    return data
-
-
 def save_json(data, output_json_path, config_path):
-    with open(output_json_path, "w") as file:
-        json.dump(data, file, indent=2)
-        print(f"\nSaved the populated defaultSession JSON to: {output_json_path}.")
-        print(f"To test the file, copy it to: {config_path.replace('.yml', '.json')} and run the makefile (make all).")
-        print(
-            "\nNote! The settings for which scaffold to display as default were set based on the first scaffold in the FASTA."
-        )
-        print("bgPerPx often needs manual adjustment based on the length of the scaffold and gene annotation density.")
-        print("If the first scaffold is not the one you want to display by default, this need to be adjusted manually.")
+    """
+    Subfunction that writes a dictionary (data) as a JSON file at the specified output path.
+    """
+    try:
+        with open(output_json_path, "w") as file:
+            json.dump(data, file, indent=2)
+            print(f"\nSaved the populated defaultSession JSON to: {output_json_path}.")
+            print(
+                f"To test the file, copy it to: {config_path.replace('.yml', '.json')} and run the makefile (make all)."
+            )
+            print(
+                "\nNote! The settings for which scaffold to display as default were set based on the first scaffold in the FASTA."
+            )
+            print(
+                "bgPerPx often needs manual adjustment based on the length of the scaffold and gene annotation density."
+            )
+            print(
+                "If the first scaffold is not the one you want to display by default, this need to be adjusted manually."
+            )
+    except IOError as e:
+        print(f"Error: Failed to save JSON file. Error: {e}")
 
 
 def main():
+    """
+    Main function. Reads config.yml, extracts commonly used variables, and then calls on subfunctions to populate the
+    defaultSession object accordingly and to eventually save the resulting JSON file.
+    """
     args = parse_arguments()
     config_path = args.yaml
     config_dir = os.path.dirname(config_path)
-    with open(config_path, "r") as file:
-        configs = list(yaml.safe_load_all(file))
+    try:
+        with open(config_path, "r") as file:
+            configs = list(yaml.safe_load_all(file))
+    except FileNotFoundError:
+        print(f"Error: The file {config_path} was not found.")
+        return
+    except yaml.YAMLError as e:
+        print(f"Error: Failed to parse YAML file {config_path}. Error: {e}")
+        return
 
     git_root = get_git_repo_root()
 
@@ -413,9 +473,11 @@ def main():
             raise ValueError(
                 f"Document {assembly_counter+1} in the config.yml is empty. Each document must contain data. Exiting."
             )
-        # The organism key is intended to be present once in the config.yml file, and thus it is considered part of the first YAML document.
-        # The below logic is dependent on there not being multiple organism keys in the same config.yml file.
-        if "organism" in config and assembly_counter == 0:
+        if assembly_counter == 0:
+            if "organism" not in config or not config["organism"]:
+                raise ValueError(
+                    f"The primary assembly (assembly number {assembly_counter+1} in config.yml) is required to have a non-empty 'organism' key. Exiting."
+                )
             species_info = {
                 "species_name": config["organism"],
                 "species_abbreviation": get_species_abbreviation(config["organism"]),
@@ -427,7 +489,6 @@ def main():
                     git_root,
                     f"scripts/data_stewardship/temp/{species_info['species_name_underscored']}_default_session.json",
                 )
-
             populated_data = {}
             populated_data = populate_defaultSession_object(populated_data, species_info)
 
