1234a7eac9693caa250e16a91a6a591cef8dde09
RMCrean <RMCrean@users.noreply.github.com>
2025-01-13 11:43:01 +0100

    Automatically render of recaptcha widget
    Simplify code by using "Automatically render the reCAPTCHA widget" instead of explicity rendering approach
    see docs: https://developers.google.com/recaptcha/docs/display

--- hugo/assets/js/recaptcha.js
+++
@@ -1,8 +0,0 @@
-/*
-Google reCAPTCHA v2 callback
-*/
-var onloadCallback = function () {
-    grecaptcha.render(id = "recaptcha-widget", {
-        'sitekey': '6LfLRQMqAAAAACSectjtigWkNvif1G2J8EFlj1nV'
-    });
-};
