6fd91bd2c7a9d90504998e1b595984696f6f28c8
Daniel P Brink <daniel.brink@scilifelab.se>
2025-05-19 14:56:27 +0200

    Update plugin to link to InterProScan member databases when possible
    InterProScan aggregates and archives several dbxref dbs, so by linking here instead resolves some of the issues found when linking directly to some of the databases

--- hugo/static/custom_jbrowse_plugins/dbxref_plugin.js
+++ hugo/static/custom_jbrowse_plugins/dbxref_plugin.js
@@ -18,50 +18,60 @@ export default class DbxrefPlugin {
         if (dbxref.startsWith('InterPro:')) {
           const ref = dbxref.replace('InterPro:', '')
           return `<a href="https://www.ebi.ac.uk/interpro/entry/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('Pfam:')) {
-          const ref = dbxref.replace('Pfam:', '')
-          return `<a href="https://pfam.xfam.org/family/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('ProSiteProfiles:')) {
-          const ref = dbxref.replace('ProSiteProfiles:', '')
-          return `<a href="https://prosite.expasy.org/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('ProSitePatterns:')) {
-          const ref = dbxref.replace('ProSitePatterns:', '')
-          return `<a href="https://prosite.expasy.org/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('SMART:')) {
-          const ref = dbxref.replace('SMART:', '')
-          return `<a href="https://smart.embl.de/smart/do_annotation.pl?DOMAIN=${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('Gene3D:')) {
-          const ref = dbxref.replace('Gene3D:', '')
-          return `<a href="http://www.cathdb.info/version/latest/superfamily/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('SUPERFAMILY:')) {
-          const ref = dbxref.replace('SUPERFAMILY:', '')
-          return `<a href="https://supfam.org/SUPERFAMILY/cgi-bin/scop.cgi?ipid=${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('PANTHER:')) {
-          const ref = dbxref.replace('PANTHER:', '')
-          return `<a href="http://www.pantherdb.org/panther/family.do?clsAccession=${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('PRINTS:')) {
-          const ref = dbxref.replace('PRINTS:', '')
-          return `<a href="https://www.bioinf.manchester.ac.uk/cgi-bin/dbbrowser/PRINTS/DoGET.pl?ac=${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('TIGRFAMs:')) {
-          const ref = dbxref.replace('TIGRFAMs:', '')
-          return `<a href="https://tigrfams.jcvi.org/cgi-bin/HmmReportPage.cgi?acc=${ref}" target="_blank">${dbxref}</a>`
         } else if (dbxref.startsWith('CDD:')) {
           const ref = dbxref.replace('CDD:', '')
-          return `<a href="https://www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid=${ref}" target="_blank">${dbxref}</a>`
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/cdd/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('FunFam:')) {
+          // Example: FunFam:G3DSA:2.60.40.60:FF:000066
+          const parts = dbxref.split(':')
+          if (
+            parts.length === 5 &&
+            parts[1] === 'G3DSA' &&
+            parts[3] === 'FF'
+          ) {
+            const superfamily = parts[2]
+            const funfam = String(Number(parts[4]))
+            return `<a href="https://www.cathdb.info/version/v4_4_0/superfamily/${superfamily}/funfam/${funfam}" target="_blank">${dbxref}</a>`
+          } else {
+            return dbxref
+          }
+        } else if (dbxref.startsWith('Gene3D:')) {
+          const ref = dbxref.replace('Gene3D:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/cathgene3d/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('Hamap:')) {
+          const ref = dbxref.replace('Hamap:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/hamap/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('PANTHER:')) {
+          const ref = dbxref.replace('PANTHER:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/panther/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('Pfam:')) {
+          const ref = dbxref.replace('Pfam:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/pfam/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('PIRSF:')) {
+          const ref = dbxref.replace('PIRSF:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/pirsf/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('PRINTS:')) {
+          const ref = dbxref.replace('PRINTS:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/prints/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('ProSitePatterns:')) {
+          const ref = dbxref.replace('ProSitePatterns:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/prosite/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('ProSiteProfiles:')) {
+          const ref = dbxref.replace('ProSiteProfiles:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/profile/${ref}" target="_blank">${dbxref}</a>`
         } else if (dbxref.startsWith('SFLD:')) {
           const ref = dbxref.replace('SFLD:', '')
-          return `<a href="https://sfld.rbvi.ucsf.edu/django/ontology/family/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('MobiDBLite:')) {
-          const ref = dbxref.replace('MobiDBLite:', '')
-          return `<a href="https://mobidb.bio.unipd.it/entries/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('FunFam:')) {
-          const ref = dbxref.replace('FunFam:', '')
-          return `<a href="http://www.cathdb.info/version/latest/superfamily/${ref}" target="_blank">${dbxref}</a>`
-        } else if (dbxref.startsWith('Coils:')) {
-          // Coils does not have a direct URL, return plaintext
-          return dbxref
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/sfld/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('SMART:')) {
+          const ref = dbxref.replace('SMART:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/smart/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('SUPERFAMILY:')) {
+          const ref = dbxref.replace('SUPERFAMILY:', '')
+          return `<a href="https://www.ebi.ac.uk/interpro/entry/ssf/${ref}" target="_blank">${dbxref}</a>`
+        } else if (dbxref.startsWith('TIGRFAM:')) {
+          const ref = dbxref.replace('TIGRFAM:', '')
+          return `<a href="https://tigrfams.jcvi.org/cgi-bin/HmmReportPage.cgi?acc=${ref}" target="_blank">${dbxref}</a>`
         }
-
         // Default: return plaintext if no link is defined
         return dbxref
       }).join('<br>')
@@ -76,7 +86,10 @@ export default class DbxrefPlugin {
 
       return goTerms.map(ontology_term => {
         const ref = ontology_term.replace('GO:', '')
-        return `<a href="https://amigo.geneontology.org/amigo/term//GO:${ref}" target="_blank">${ontology_term}</a>`
+        if (ref === '-') {
+          return ref
+        }
+        return `<a href="https://amigo.geneontology.org/amigo/term/GO:${ref}" target="_blank">${ontology_term}</a>`
       }).join('<br>')
     })
 
